e97ea7980313f1fe1b874797ff1a3b776d53d636
azharkih <azharkih@gmail.com>
2021-07-05 20:57:04 +0300

    complete

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# yamdb_final
-yamdb_final
+![workflow](https://github.com/azharkih/yamdb_final/actions/workflows/main.yml/badge.svg)
+# API yamdb
+
+## Краткое описание проекта
+API предназначено для обмена информацией об оценках произведений пользователями 
+и комментариев этих оценок
+
+### Запуск приложения
+Для запуска приложения необходимо ввести следующие команды:
+```
+docker-compose up
+docker-compose exec web python manage.py migrate --noinput
+docker-compose exec web python manage.py collectstatic --no-input
+```
+
+### Создание суперпользователя
+Для создания суперпользователя необходимо ввести следующую команду:
+```
+docker-compose exec web python manage.py createsuperuser
+```
+
+### Заполнение базы начальными данными
+Для наполнения базы начальными данными выполнить команду:
+```
+docker-compose exec web python manage.py loaddata fixtures.json
+```
---
+++ api/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+
+from .models import Comment, Category, Genre, Title, Review
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    empty_value_display = '-пусто-'
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    empty_value_display = '-пусто-'
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year', 'description', 'category')
+    empty_value_display = '-пусто-'
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('author', 'title', 'score', 'text', 'pub_date')
+    empty_value_display = '-пусто-'
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'review', 'text', 'pub_date')
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Title, TitleAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Comment, CommentAdmin)
